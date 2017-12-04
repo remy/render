a30080642884cb1cc522468263af4989b6171c3c
Remy Sharp <remy@remysharp.com>
2017-12-04 08:14:02 +0000

    fix: bump to latest across the board

--- js/app.js
+++ js/app.js
@@ -1,56 +1,67 @@
+function require(source) {
+  const module = require.cache[source + '.js'];
+  const exports = {};
+  module(exports, exports);
+  return exports;
+}
+
+require.cache = {};
+
+require.register = (source, callback) => {
+  require.cache[source] = callback;
+};
+
 require.register('markdown.js', function(module, exports) {
   exports.parse = markdown.toHTML;
-  exports.render = function (source, locals, callback) {
+  exports.render = function(source, locals, callback) {
     callback(markdown.toHTML(source));
-  }
+  };
 });
 
-var jade = require('jade');
-
-require.register('jade.js', function (module, exports) {
-  exports.render = function (source, locals, callback) {
+require.register('jade.js', function(module, exports) {
+  exports.render = function(source, locals, callback) {
     var fn = jade.compile(source, { pretty: true });
     callback(fn(locals));
   };
 });
 
-require.register('mustache.js', function (module, exports) {
-  exports.render = function (source, locals, callback) {
+require.register('mustache.js', function(module, exports) {
+  exports.render = function(source, locals, callback) {
     callback(Mustache.to_html(source, locals));
-  }
+  };
 });
 
-require.register('handlebars.js', function (module, exports) {
-  exports.render = function (source, locals, callback) {
+require.register('handlebars.js', function(module, exports) {
+  exports.render = function(source, locals, callback) {
     var template = Handlebars.compile(source);
     callback(template(locals));
-  }
+  };
 });
 
-require.register('plates.js', function (module, exports) {
-  exports.render = function (source, locals, callback) {
+require.register('plates.js', function(module, exports) {
+  exports.render = function(source, locals, callback) {
     callback(Plates.bind(source, locals));
-  }
+  };
 });
 
-require.register('jsrender.js', function (module, exports) {
-  exports.render = function (source, locals, callback) {
+require.register('jsrender.js', function(module, exports) {
+  exports.render = function(source, locals, callback) {
     $.template('__render', source);
     var html = $.render(locals, '__render');
     callback(html);
   };
 });
 
-require.register('underscore.js', function (module, exports) {
-  exports.render = function (source, locals, callback) {
+require.register('lodash.js', function(module, exports) {
+  exports.render = function(source, locals, callback) {
     callback(_.template(source)(locals));
   };
 });
 
-require.register('less.js', function (module, exports) {
-  var less = new window.less.Parser;
-  exports.render = function (source, locals, callback) {
-    less.parse(source, function (err, tree) {
+require.register('less.js', function(module, exports) {
+  var less = new window.less.Parser();
+  exports.render = function(source, locals, callback) {
+    less.parse(source, function(err, tree) {
       if (err) {
         var e = new Error();
         e.type = err.name;
@@ -63,59 +74,70 @@ require.register('less.js', function (module, exports) {
   };
 });
 
-require.register('coffee-script.js', function (module, exports) {
-  exports.render = function (source, locals, callback) {
-    callback(CoffeeScript.compile(source, { bare:true }));
-  };
-});
-
 var parsers = {},
-    lang = 'jade',
-    languages = ['Coffee-Script', 'Jade', 'Markdown', 'Mustache', 'LESS', 'JsRender', 'Handlebars', 'Plates', 'Underscore'].sort(function (a, b) { return a.toLowerCase() > b.toLowerCase() }),
-    locals = {},
-    qs = getQueryString();
+  lang = 'jade',
+  languages = [
+    'Jade',
+    'Markdown',
+    'Mustache',
+    'LESS',
+    'JsRender',
+    'Handlebars',
+    'Plates',
+    'Lodash',
+  ].sort(function(a, b) {
+    return a.toLowerCase() > b.toLowerCase();
+  }),
+  locals = {},
+  qs = getQueryString();
 
-languages.forEach(function (lang) {
+languages.forEach(function(lang) {
   lang = lang.toLowerCase();
   parsers[lang] = require(lang);
 });
 
 function getQueryString() {
-  var result = {}, queryString = location.search.substring(1),
-      re = /([^&=]+)=([^&]*)/g, m;
+  var result = {},
+    queryString = location.search.substring(1),
+    re = /([^&=]+)=([^&]*)/g,
+    m;
 
-  while (m = re.exec(queryString)) {
+  while ((m = re.exec(queryString))) {
     result[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
   }
 
   return result;
 }
 
-
 function throttle(fn, delay) {
   var timer = null;
-  return function () {
-    var context = this, args = arguments;
+  return function() {
+    var context = this,
+      args = arguments;
     clearTimeout(timer);
-    timer = setTimeout(function () {
+    timer = setTimeout(function() {
       fn.apply(context, args);
     }, delay);
   };
 }
 
-jQuery(function ($) {
+jQuery(function($) {
   $.event.special.updated = {
-    setup: function (data, namespaces) {
-      $(this).on('keyup', keyup).data('last', this.value);
+    setup: function(data, namespaces) {
+      $(this)
+        .on('keyup', keyup)
+        .data('last', this.value);
     },
-    teardown: function () {
-      $(this).off('keyup', keyup).removeData('last');
-    }
+    teardown: function() {
+      $(this)
+        .off('keyup', keyup)
+        .removeData('last');
+    },
   };
 
   function keyup(event) {
     var last = $.data(this, 'last'),
-        content = this.value;
+      content = this.value;
     if (content !== last) {
       $(this).trigger('updated');
       last = content;
@@ -124,25 +146,28 @@ jQuery(function ($) {
   }
 });
 
-$(function () {
+$(function() {
   var $render = $('#render'),
-      $source = $('#source'),
-      $error = $('pre p'),
-      $locals = $('#locals'),
-      $lang = $('#lang'),
-      last = null;
+    $source = $('#source'),
+    $error = $('pre p'),
+    $locals = $('#locals'),
+    $lang = $('#lang'),
+    last = null;
 
   var html = '';
-  languages.forEach(function (lang) {
+  languages.forEach(function(lang) {
     html += '<option value="' + lang.toLowerCase() + '">' + lang + '</option>';
   });
 
-  $lang.html(html).change(function () {
-    lang = this.value;
-    update();
-  }).val('jade');
+  $lang
+    .html(html)
+    .change(function() {
+      lang = this.value;
+      update();
+    })
+    .val('jade');
 
-  var $showLocals = $('#showlocals').click(function () {
+  var $showLocals = $('#showlocals').click(function() {
     $locals[this.checked ? 'show' : 'hide']();
   });
 
@@ -159,41 +184,59 @@ $(function () {
   }
 
   if (qs.source) $source.val(qs.source);
-  if (qs.lang) { $lang.val(qs.lang); lang = qs.lang; }
-  if (qs.locals) { $locals.val(qs.locals); $showLocals[0].checked = true; $locals.show(); }
+  if (qs.lang) {
+    $lang.val(qs.lang);
+    lang = qs.lang;
+  }
+  if (qs.locals) {
+    $locals.val(qs.locals);
+    $showLocals[0].checked = true;
+    $locals.show();
+  }
 
-  window.getpath = function () {
-    return '?source=' + encodeURIComponent($source.val()) + '&lang=' + encodeURIComponent(lang) + '&locals=' + encodeURIComponent($locals.val());
+  window.getpath = function() {
+    return (
+      '?source=' +
+      encodeURIComponent($source.val()) +
+      '&lang=' +
+      encodeURIComponent(lang) +
+      '&locals=' +
+      encodeURIComponent($locals.val())
+    );
   };
 
   update();
 
-  $(window).unload(function () {
+  $(window).unload(function() {
     localStorage.config = JSON.stringify({
       lang: $lang.val(),
       source: $source.val(),
       locals: $locals.val(),
-      showLocals: $showLocals.is(':checked')
+      showLocals: $showLocals.is(':checked'),
     });
   });
 
   var historyTimer = null;
   function update() {
-    var source = $source.val(), ok = false, error = '';
+    var source = $source.val(),
+      ok = false,
+      error = '';
     clearTimeout(historyTimer);
-    setTimeout(function () {
+    setTimeout(function() {
       window.history.pushState('', '', getpath());
     }, 1000);
 
     try {
       try {
-        locals = (new Function('var alert = prompt = confirm = function(){}; return ' + $locals.val()))();
+        locals = new Function(
+          'var alert = prompt = confirm = function(){}; return ' + $locals.val()
+        )();
       } catch (e) {
         throw new Error('Cannot parse locals - try valid JSON');
       }
 
       if (parsers[lang]) {
-        parsers[lang].render(source, locals, function (renderedSource) {
+        parsers[lang].render(source, locals, function(renderedSource) {
           ok = true;
           $render.parent().removeClass('error');
           $render.text(renderedSource);
